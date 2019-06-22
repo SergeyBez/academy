eced387b9120a7bcbd94f63e0cf6b49dc0958530
sergeibeza <sergey.b@thewhitelabelagency.com>
2019-06-22 08:58:16 +0300

    Create file index.php
    Create main index.php file

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Academy</title>
+</head>
+<body>
+	<ol>
+		<li>1. Create file on repository</li>
+	</ol>		
+</body>
+</html>
